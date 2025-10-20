e4660ae803cb09558669d45fbdb51add9d403786
Carlos Alberto da Silva Neto <netoc4533@gmail.com>
2025-10-19 23:27:50 -0300

    Modularização e Pacote
    Fiquei muito tempo tentando entender por completo esse assunto, porém ainda não tenho domínio. Pretendo fazer uma reformulação nesse assunto!

---
+++ Gustavo Guanabara/Prática/Módulos/uteis/numeros/__init__.py
@@ -0,0 +1,15 @@
+def fatorial_de_um_número(num=1):
+  valor_final = 1
+  for n in range(1, num + 1):
+    valor_final * n
+  return valor_final
+
+
+def dobro(num):
+  return num * 2
+
+
+def triplo(num):
+  return num * 3
+
+# Aqui é toda a função do meu código, está nesse arquivo de __init__.py.
